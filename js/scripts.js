39b8a136350f3ed7c79535222dc2cf597bdfe880
Newton Bii <biinewton382@gmail.com>
2017-10-18 15:17:26 +0300

    Add preventDefault function.

--- js/scripts.js
+++ js/scripts.js
@@ -9,8 +9,11 @@ var divide = function(number1, number2) {return (number1/number2); };
 //Business logic ends here
 //Front end logic begins after.
 $(document).ready(function(){
+  $("form#add").submit(function(event) {
+    event.preventDefault();
   var number1 = parseInt($("#add1").val());
   var number2 = parseInt($("#add2").val());
   alert(add(number1, number2));
+  });
 
 });
