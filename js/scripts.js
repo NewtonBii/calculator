f54cba686df57a6b3d18f1398204e022f2ea0e6f
Newton Bii <biinewton382@gmail.com>
2017-10-18 15:00:59 +0300

    Add front end logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,17 @@
 var add = function(number1, number2) {return(number1+number2);};
 
-var number1 = parseInt(prompt("Enter a number"));
-var number2 = parseInt(prompt("Enter another number"));
-alert("Addition result " + add(number1, number2));
-
 var subtract = function(number1,number2) {return (number1-number2);};
-
-var number1 = parseInt(prompt("Enter a number"));
-var number2 = parseInt(prompt("Enter another number"));
-alert("Subtraction result " + subtract(number1, number2));
 
 var multiply = function(number1, number) {return (number1*number2); };
 
-var number1 = parseInt(prompt("Enter a number"));
-var number2 = parseInt(prompt("Enter another number"));
-alert("Multiplication result " + multiply(number1, number2));
-
 var divide = function(number1, number2) {return (number1/number2); };
 
-var number1 = parseInt(prompt("Enter a number"));
-var number2 = parseInt(prompt("Enter another number"));
-alert("Division result " + divide(number1, number2));
+//Business logic ends here
+//Front end logic begins after.
+$(document).ready(function(){
+  var number1 = parseInt(prompt("Enter a number"));
+  var number2 = parseInt(prompt("Enter another number"));
+
+  alert(add(number1, number2));
+
+});
